686c556185d780880fa7f198bd7b097516f317bf
MuteAim <ji_hyunsoo@naver.com>
2024-12-31 16:32:55 +0900

    feat: 메인페이지에 게시판 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 "use client"
 
 import Hero from "./features/easilyMain/hero";
+import EasilyPosts from "./features/posts/page";
 
 export default function Home() {
   return (
+    <body>
       <Hero />
+      <EasilyPosts />
+    </body>
   );
 }
